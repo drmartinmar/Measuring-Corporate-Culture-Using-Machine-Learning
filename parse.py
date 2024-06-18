b3542b182cc5cce415fb7198b9392ed2e99ee8a9
drmartinmar <62364689+drmartinmar@users.noreply.github.com>
2024-06-18 13:04:26 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -94,10 +94,10 @@ def process_largefile(
                 output_line_ids.append(output_line_id)
             output_lines = "\n".join(output_lines) + "\n"
             output_line_ids = "\n".join(output_line_ids) + "\n"
-            with open(output_file, "a", newline="\n") as f_out:
+            with open(output_file, "a", encoding="utf-8", newline="\n") as f_out:
                 f_out.write(output_lines)
             if output_index_file is not None:
-                with open(output_index_file, "a", newline="\n") as f_out:
+                with open(output_index_file, "a", encoding="utf-8", newline="\n") as f_out:
                     f_out.write(output_line_ids)
 
 
